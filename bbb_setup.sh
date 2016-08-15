f1a41d2dc8ca45d94bae550614bb620bda4acbc6
awong36 <little.rice.man@gmail.com>
2016-08-15 12:28:00 -0400

    bbb setup script

---
+++ bbb_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+apt-get update
+apt-get -y upgrade
+apt-get -y install ntp
+apt-get -y install cython
+apt-get -y install usbmount
+apt-get -y install dos2unix
+apt-get -y install lsof
+mv /etc/localtime /etc/localtime.old
+ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+pip install minimalmodbus
+pip install modbus-tk
