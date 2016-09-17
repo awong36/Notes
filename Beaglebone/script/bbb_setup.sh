b3a5c75c929a0c0b4fe3761ebbf2802e4beba0be
awong36 <little.rice.man@gmail.com>
2016-09-16 22:31:30 -0400

    notes update

--- Beaglebone/script/bbb_setup.sh
+++ Beaglebone/script/bbb_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "=========== Device Update ==========="
 apt-get update
 apt-get -y upgrade
 apt-get -y install ntp
@@ -7,9 +8,10 @@ apt-get -y install cython
 apt-get -y install usbmount
 apt-get -y install dos2unix
 apt-get -y install lsof
-echo "Date Modification..."
+echo "=========== Date Modification ==========="
 mv /etc/localtime /etc/localtime.old
 ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
 date
+echo "=========== Python Library Setup ==========="
 pip install minimalmodbus
 pip install modbus-tk
